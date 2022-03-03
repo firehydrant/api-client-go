270ae21224b3aac9a8e3ee7452ef82c7fea399f0
Brian Morton <bmorton@firehydrant.io>
2022-03-03 10:50:00 -0800

    Allow HTTP transport to be injected for telemetry

--- fhclient/main.go
+++ fhclient/main.go
@@ -3,15 +3,17 @@ package fhclient
 import (
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/strfmt"
+	"net/http"
 
 	client "github.com/firehydrant/api-client-go/client"
 	transport "github.com/go-openapi/runtime/client"
 )
 
 type Config struct {
-	ApiHost string
-	ApiKey  string
-	Debug   bool
+	ApiHost   string
+	ApiKey    string
+	Debug     bool
+	Transport http.RoundTripper
 }
 
 type ApiClient struct {
@@ -25,6 +27,9 @@ func NewApiClient(c Config) ApiClient {
 
 	fhApiClient.transport = transport.New(c.ApiHost, "", []string{"https"})
 	fhApiClient.transport.Debug = c.Debug
+	if c.Transport != nil {
+		fhApiClient.transport.Transport = c.Transport
+	}
 
 	fhApiClient.Client = client.New(fhApiClient.transport, strfmt.Default)
 	fhApiClient.Auth = transport.BearerToken(c.ApiKey)
