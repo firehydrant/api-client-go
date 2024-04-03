8bb105c478c164d5f46448503a065cede44ca13e
Wilson E. Husin <whusin@firehydrant.com>
2024-04-03 10:39:24 -0700

    Debug our working directory

--- bin/regenerate.sh
+++ bin/regenerate.sh
@@ -5,8 +5,15 @@ set -eu
 # Get root of repository.
 repo_root=$(git rev-parse --show-toplevel)
 
+set -x
+
+pwd
+
 pushd "${repo_root}" > /dev/null
+  pwd
   bin/deps.sh github.com/go-swagger/go-swagger 
+  ls bin
+  swagger version
 
   rm -rf client models
   bin/swagger generate client -f swagger_doc.json
